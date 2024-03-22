b111b8e113b3aae3b86712f4eb9dfeaa8ee94aec
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-03-22 13:00:58 -0700

    Add files via upload

---
+++ iknowthisone.py
@@ -0,0 +1,10 @@
+
+import html.entities
+
+html.entities.__all__.__reversed__
+
+try:
+    import _curses
+    _curses.__path__.__hash__.__class__.__code__.co_consts.count
+except:
+    html.entities.__spec__.has_location.real.to_bytes.__text_signature__.__doc__
